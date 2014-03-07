3c139e91951392406c730f76f425235d163f2097
Chris <chris.haack@me.com>
2014-03-07 09:53:45 -0500

    create trips controller

--- app/models/parser.rb
+++ app/models/parser.rb
@@ -5,7 +5,6 @@ class Parser < ActiveRecord::Base
 	attr_accessor :output
 	attr_reader :columns, :rows
 
-#["Carrier Code", "Date (MM/DD/YYYY)", "Flight Number", "Tail Number", "Origin Airport ", "Scheduled Arrival Time", "Actual Arrival Time", nil]
 	def initialize(path_to_csv)
 		flights_csv = CSV.read(path_to_csv)
 		@columns = flights_csv.first
@@ -28,7 +27,7 @@ class Parser < ActiveRecord::Base
 	end
 
 #["Carrier Code", "Date (MM/DD/YYYY)", "Flight Number", "Tail Number", "Origin Airport ", "Scheduled Arrival Time", "Actual Arrival Time", nil]
-	def add_to_database(destination_id)
+	def add_to_database(orgin_id)
 		parse_flights.each do |hash|
 			hash.each do |attribute, value|
 			airline_object = Airline.create(:name => hash["Carrier Code"])
@@ -47,26 +46,11 @@ class Parser < ActiveRecord::Base
 			departure_object = Departure.create(
 				:date => hash["Date (MM/DD/YYYY)"]
     		:flight_id => flight_object.id
-    		:airport_id => destination_id
+    		:airport_id => origin_id
 				)
-			airport_object
 			end
 		end
 	end
-
-
-# {"Carrier Code"=>"UA", 
-#     "Date (MM/DD/YYYY)"=>"12/31/2013",
-#     Flights 
-#     "Flight Number"=>"1686", 
-#    # "Tail Number"=>"N24729", 
-#    Airports
-#     "Origin Airport "=>"SJU", 
-#     Arrivals
-#     "Scheduled Arrival Time"=>"19:20", 
-#     "Actual Arrival Time"=>"19:37"
-# }
-
 
 end
 
