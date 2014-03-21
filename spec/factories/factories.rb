66b4ad0a7b6329a564cba438e2a08e75313b2a92
Chris <chris.haack@me.com>
2014-03-21 12:58:34 -0400

    add tests for 'name' and 'flights' instance methods, add context 'valid attributes'

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,5 +1,12 @@
 FactoryGirl.define do
+  
   factory :airline do
     name "example"
   end
+
+  factory :flight do
+    number 0
+    airline_id 0
+  end
+
 end
--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -3,15 +3,35 @@ require 'spec_helper'
 describe Airline do
   
   airline = FactoryGirl.create(:airline, :name => "Jet Blue")
+  flight1 = FactoryGirl.create(:flight, :number => 101, :airline_id => 1)
+  flight2 = FactoryGirl.create(:flight, :number => 102, :airline_id => 1)
 
-  it "is valid with a name" do
-    airline.should be_valid
+  
+  describe "#name" do
+    it "should have a name" do
+      airline.name.should == "Jet Blue"
+    end
   end
 
-  it "is not valid without a name" do
-    airline.name = nil
-    airline.should_not be_valid
+  context "valid attributes" do 
+    it "is valid with a name" do
+      airline.should be_valid
+    end
+
+    it "is not valid without a name" do
+      airline.name = nil
+      airline.should_not be_valid
+    end
   end
+
+  describe "#flights" do
+    it 'include flights' do
+      airline.flights << flight1
+      airline.flights << flight2
+      airline.flights.should (include flight1)
+      airline.flights.should (include flight2)
+    end
+  end 
 
 end
 
